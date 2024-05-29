269db4c6eaf44f5bb0aa13e16e634c7ebabbd75d
Joni Muranen <2306093@edu.karelia.fi>
2024-05-29 15:28:51 +0300

    viimeinen versio

--- osa1/anekdootit/anekdootit/src/App.jsx
+++ osa1/anekdootit/anekdootit/src/App.jsx
@@ -14,6 +14,24 @@ const VoteButton = ({onClick, text}) =>{
     </button>
   )
 }
+const MostVotes = ({anecdotes, votes}) =>{
+  const maxVotes = Math.max(...votes)
+  const maxIndex = votes.indexOf(maxVotes)
+if(maxVotes === 0){
+  return <div>No votes yet</div>
+}
+return (
+  <div>
+    <h2>Anecdote with most votes</h2>
+    <div>{anecdotes[maxIndex]}</div>
+    <div>has {maxVotes} votes</div>
+  </div>
+)
+}
+
+
+
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -28,24 +46,25 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-
+  
   const selectRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   }
-
   const voteAnecdote = () => {
     const newVotes = [...votes];
     newVotes[selected] += 1;
     setVotes(newVotes);
-  };
-
+  }
+  
   return (
     <div>
+    <h1>Anecdote of the day</h1>
     <div>{anecdotes[selected]}</div>
     <div>has {votes[selected]} votes</div>
     <Button onClick={selectRandomAnecdote} text="Next anecdote" />
     <VoteButton onClick={voteAnecdote} text="Vote" />
+    <MostVotes anecdotes={anecdotes} votes={votes}/>
     </div>
   )
 }
