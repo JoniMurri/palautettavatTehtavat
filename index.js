126da1f7ec0cb06162cdca741b69ef7a9cc5b695
Joni Muranen <2306093@edu.karelia.fi>
2024-06-12 15:33:29 +0300

    commit changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
-const PORT = process.env.Port || 4000;
+const PORT = process.env.Port || 3001;
 app.use(cors())
 app.use(express.json())
 
